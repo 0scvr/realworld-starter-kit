2611ea2a8fb12efc06916c06ff15265896ff5f68
devstra <oscar.f@outlook.fr>
2021-09-26 14:55:34 +0200

    added some view templates

---
+++ RealWorldMVC/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using RealWorldMVC.Models;
+
+namespace RealWorldMVC.Services
+{
+    public interface IUserService
+    {
+        Task<User[]> GetUsersAsync();
+        Task<User> GetUserByIdAsync(Guid guid);
+        Task<User> GetUserByUsernameAsync(string username);
+        Task<Comment[]> GetUserCommentsByUsernameAsync(string username);
+        Task<Article[]> GetUserArticlesByUsernameAsync(string username);
+    }
+}
